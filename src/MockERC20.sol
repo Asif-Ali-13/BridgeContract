5eee29274594c79f9ff7c1bbb87adbdb618066ff
Asif Ali <aali9548702@gmail.com>
2025-10-22 22:17:53 +0530

    init and create mock erc20

---
+++ src/MockERC20.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    bool public failTransfers;
+
+    error BridgeContract_Transaction_Failed();
+
+    constructor() ERC20("Mock Token", "MTK") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    function setFailTransfers(bool _fail) external {
+        failTransfers = _fail;
+    }
+
+    function transfer(
+        address to, 
+        uint256 amount
+    ) public override returns (bool) {
+
+        if (failTransfers) revert BridgeContract_Transaction_Failed();
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(
+        address from, 
+        address to, 
+        uint256 amount
+    ) public override returns (bool) {
+
+        if (failTransfers) revert BridgeContract_Transaction_Failed();
+        return super.transferFrom(from, to, amount);
+    }
+}
